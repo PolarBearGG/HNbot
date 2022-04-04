11921bf3360aca9f3e7e1d289c41d4551f4f8bbc
PolarBearGG <vitaliykorolyk95@gmail.com>
2022-04-04 14:42:43 +0300

    change path

--- root/main.py
+++ root/main.py
@@ -2,7 +2,7 @@ from bot import send_message
 import pandas as pd
 
 
-df = pd.read_csv('stories.csv')
+df = pd.read_csv('/home/korolyktv/HNbot/root/stories.csv')
 
 id = df["id"].head(1)[0]
 title = df["title"].head(1)[0]
@@ -13,7 +13,7 @@ text = f'<b>{title}</b>\n\n{url}'
 send_message(text, '@PythomHN', 'HTML')
 
 df = df.drop([0])
-df.to_csv('stories.csv', index=False)
+df.to_csv('/home/korolyktv/HNbot/root/stories.csv', index=False)
 
-with open('history.txt', mode='a') as f:
+with open('/home/korolyktv/HNbot/root/history.txt', mode='a') as f:
     f.write(f'{id}\n')
--- root/parser.py
+++ root/parser.py
@@ -7,7 +7,7 @@ def parseStories(stories_id_url):
 
     df_new_top500_from_HN = pd.read_json(stories_id_url)
 
-    with open('history.txt', mode='r') as f:
+    with open('/home/korolyktv/HNbot/root/history.txt', mode='r') as f:
         file = f.read()
 
     for i, id in enumerate(df_new_top500_from_HN[0]):
@@ -17,11 +17,12 @@ def parseStories(stories_id_url):
 
         if ('python' in df_story_details_from_HN['title'][0] or 'Python' in df_story_details_from_HN['title'][0]) and str(df_story_details_from_HN['id'][0]) not in file:
             df_story_details_from_HN.to_csv(
-                'stories.csv', mode='a', index=False, header=False)
+                '/home/korolyktv/HNbot/root/stories.csv', mode='a', index=False, header=False)
 
-    df = pd.read_csv("stories.csv", on_bad_lines='skip')
+    df = pd.read_csv("/home/korolyktv/HNbot/root/stories.csv",
+                     on_bad_lines='skip')
     df.drop_duplicates(inplace=True, keep=False, )
-    df.to_csv("stories.csv", index=False)
+    df.to_csv("/home/korolyktv/HNbot/root/stories.csv", index=False)
 
 
 parseStories(stories_id_url)
