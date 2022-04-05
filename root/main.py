f18913aaf882cac4fe1be20443898471c40af2cb
PolarBearGG <vitaliykorolyk95@gmail.com>
2022-04-05 21:06:16 +0300

    Law of Demeter:parser.py

--- root/main.py
+++ root/main.py
@@ -1,22 +1,16 @@
 from bot import Bot
-from parser import Parser
+from parser import getStory, rememberStoryId
 
-p = Parser()
-p.createDB()
-p.saveNewStories()
-df = p.getStory()
+story = getStory()
 
-id = df["id"][0]
-title = df["title"][0]
-url = df["url"][0]
-
+id = story["id"][0]
+title = story["title"][0]
+url = story["url"][0]
 
 text = f'<b>{title}</b>\n\n{url}'
-
 
 b = Bot("5183911411:AAGl7yMmUE5Soj1G-ztDBd4YCyTXjkrET2o",
         "Python_HN_bot", '@PythomHN')
 b.send_message(text, "HTML")
 
-p.rememberStoryId()
-p.deleteStoryFromDB()
+rememberStoryId()
--- root/parser.py
+++ root/parser.py
@@ -2,19 +2,14 @@ import pandas as pd
 import sqlite3
 
 
-class Parser:
+def getStory():
+    conn = sqlite3.connect(
+        'pythonStories.sqlite')
+    c = conn.cursor()
 
-    def __init__(self) -> None:
-        self.stories_id_url = 'https://hacker-news.firebaseio.com/v0/newstories.json'
-        self.conn = sqlite3.connect(
-            '/home/korolyktv/HNbot/root/pythonStories.sqlite')
-
-    def createDB(self):
-        c = self.conn.cursor()
-
-        c.execute('''
+    c.execute('''
           CREATE TABLE IF NOT EXISTS stories
-          (id INTEGER, 
+          (id INTEGER,
            by TEXT,
            descendants INTEGER,
            kids BLOB,
@@ -25,36 +20,33 @@ class Parser:
            url TEXT)
           ''')
 
-        c.execute('''
+    c.execute('''
           CREATE TABLE IF NOT EXISTS history
           (id INTEGER)
           ''')
 
-        self.conn.commit()
+    conn.commit()
 
-    def saveNewStories(self):
-        df_new_top500_from_HN = pd.read_json(self.stories_id_url)
-        history = pd.read_sql("select * from history", self.conn)
-        for i, id in enumerate(df_new_top500_from_HN[0]):
-            df_story_details_from_HN = pd.read_json(
-                f'https://hacker-news.firebaseio.com/v0/item/{id}.json', orient='index').T
+    df_new_top500_from_HN = pd.read_json(
+        'https://hacker-news.firebaseio.com/v0/newstories.json')
+    history = pd.read_sql("select * from history", conn)
+    for i, id in enumerate(df_new_top500_from_HN[0]):
+        df_story_details_from_HN = pd.read_json(
+            f'https://hacker-news.firebaseio.com/v0/item/{id}.json', orient='index').T
 
-            if ('python' in df_story_details_from_HN['title'][0] or 'Python' in df_story_details_from_HN['title'][0]) and (len(history) == 0 or history['id'].astype(str).str.contains(str(df_story_details_from_HN['id'][0])).any() == False):
-                df_story_details_from_HN.to_sql(
-                    'stories', self.conn, if_exists='append', index=False)
+        if ('python' in df_story_details_from_HN['title'][0] or 'Python' in df_story_details_from_HN['title'][0]) and (len(history) == 0 or history['id'].astype(str).str.contains(str(df_story_details_from_HN['id'][0])).any() == False):
+            df_story_details_from_HN.to_sql(
+                'stories', conn, if_exists='append', index=False)
 
-    def getStory(self):
-        df = pd.read_sql('select * from stories limit 1', self.conn)
-        return df
+    df = pd.read_sql('select * from stories limit 1', conn)
+    return df
 
-    def rememberStoryId(self):
-        df = self.getStory()
-        c = self.conn.cursor()
-        c.execute("INSERT INTO history VALUES (?)", (int(df['id'][0]),))
-        self.conn.commit()
 
-    def deleteStoryFromDB(self):
-        df = self.getStory()
-        c = self.conn.cursor()
-        c.execute("DELETE FROM stories WHERE id = (?)", (int(df['id'][0]),))
-        self.conn.commit()
+def rememberStoryId():
+    conn = sqlite3.connect(
+        'pythonStories.sqlite')
+    df = pd.read_sql('select * from stories limit 1', conn)
+    c = conn.cursor()
+    c.execute("INSERT INTO history VALUES (?)", (int(df['id'][0]),))
+    c.execute("DELETE FROM stories WHERE id = (?)", (int(df['id'][0]),))
+    conn.commit()
