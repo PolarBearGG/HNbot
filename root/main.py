922864ed3a650dcb214f41bdca6ae9b555275001
PolarBearGG <vitaliykorolyk95@gmail.com>
2022-04-05 21:10:44 +0300

    if name == main:main.py

--- root/main.py
+++ root/main.py
@@ -1,16 +1,23 @@
 from bot import Bot
 from parser import getStory, rememberStoryId
 
-story = getStory()
 
-id = story["id"][0]
-title = story["title"][0]
-url = story["url"][0]
+def main():
 
-text = f'<b>{title}</b>\n\n{url}'
+    story = getStory()
 
-b = Bot("5183911411:AAGl7yMmUE5Soj1G-ztDBd4YCyTXjkrET2o",
-        "Python_HN_bot", '@PythomHN')
-b.send_message(text, "HTML")
+    id = story["id"][0]
+    title = story["title"][0]
+    url = story["url"][0]
 
-rememberStoryId()
+    text = f'<b>{title}</b>\n\n{url}'
+
+    b = Bot("5183911411:AAGl7yMmUE5Soj1G-ztDBd4YCyTXjkrET2o",
+            "Python_HN_bot", '@PythomHN')
+    b.send_message(text, "HTML")
+
+    rememberStoryId()
+
+
+if __name__ == "__main__":
+    main()
